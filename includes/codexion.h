9f31b54b0f06f7ed30e5c592bd4c1749547ac5c3
Jilan Benhassine <jbenhass@z4r4p6.42lyon.fr>
2026-06-08 15:05:57 +0200

    Fix - No more data race and syscall before lock

--- includes/codexion.h
+++ includes/codexion.h
@@ -6,7 +6,7 @@
 /*   By: jbenhass <jbenhass@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 16:53:21 by jbenhass          #+#    #+#             */
-/*   Updated: 2026/06/04 16:31:38 by jbenhass         ###   ########lyon.fr   */
+/*   Updated: 2026/06/05 18:17:39 by jbenhass         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,9 @@ int						init_sim(t_sim *sim, t_args *args);
 
 // UTILS.C
 unsigned long long		get_ms(unsigned long long sim_start);
-void					log_state(t_sim *sim, int coder_id, const char *msg);
+void					log_state(t_sim *sim, unsigned long long sim_start,
+							int coder_id, const char *msg);
+
 // SCHEDULER.C
 void					*scheduler_routine(void *args);
 
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jbenhass <jbenhass@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 19:09:38 by jbenhass          #+#    #+#             */
-/*   Updated: 2026/06/04 14:51:50 by jbenhass         ###   ########lyon.fr   */
+/*   Updated: 2026/06/08 15:04:11 by jbenhass         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@ unsigned long long	get_ms(unsigned long long sim_start)
 	return (t - sim_start);
 }
 
-void	log_state(t_sim *sim, int coder_id, const char *msg)
+void	log_state(t_sim *sim, unsigned long long sim_start, int coder_id,
+		const char *msg)
 {
 	unsigned long long	curr_time;
 
+	curr_time = get_ms(sim_start);
 	pthread_mutex_lock(&sim->log_mutex);
-	curr_time = get_ms(sim->sim_start);
 	printf("%lld %d %s\n", curr_time, coder_id, msg);
 	pthread_mutex_unlock(&sim->log_mutex);
 }
